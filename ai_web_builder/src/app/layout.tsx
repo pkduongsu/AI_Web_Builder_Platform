ddf30738bec5fb59f0e691529c92ce7b53cdee1b
Kim Pham <pkduongsu@gmail.com>
2025-07-14 23:35:24 +1000

    Add background job demo

--- ai_web_builder/src/app/layout.tsx
+++ ai_web_builder/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
+import {Toaster} from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         {children}
-      </body>
+      </body> 
     </html>
     </TRPCReactProvider>
   );

4f28603d04fa10353a572ef09d25644544701043
Kim Pham <pkduongsu@gmail.com>
2025-07-28 14:04:10 +1000

    fix bugs in message container, add Home Page features

--- ai_web_builder/src/modules/projects/ui/components/messages-container.tsx
+++ ai_web_builder/src/modules/projects/ui/components/messages-container.tsx
@@ -21,6 +21,7 @@ export const MessagesContainer = ({
     //adding interaction that scrolls down to the bottom every reload:
     const trpc = useTRPC();
     const bottomRef = useRef<HTMLDivElement>(null);
+    const lastAssistantMessageIdRef = useRef<string | null>(null);
 
     const {data: messages} = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId: projectId, 
@@ -29,16 +30,20 @@ export const MessagesContainer = ({
         refetchInterval: 5000,
     }));
 
-    //TODO: FIX this feature
-    // useEffect(() => {
-    //     const lastAssistantMessageWithFragment = messages.findLast(
-    //         (message) => message.role === "ASSISTANT" && !!message.fragment,
-    //     );
+    useEffect(() => {
+        const lastAssistantMessage = messages.findLast(
+            (message) => message.role === "ASSISTANT"
+        );
 
-    //     if (lastAssistantMessageWithFragment) {
-    //         setActiveFragment(lastAssistantMessageWithFragment.fragment);
-    //     }
-    // }, [messages, setActiveFragment]);
+        if (
+            lastAssistantMessage?.fragment && 
+            lastAssistantMessage.id !== lastAssistantMessageIdRef.current
+        ) {
+            setActiveFragment(lastAssistantMessage.fragment);
+            lastAssistantMessageIdRef.current = lastAssistantMessage.id;
+        }
+
+    }, [messages, setActiveFragment]);
 
     useEffect(() => {
         bottomRef.current?.scrollIntoView();
